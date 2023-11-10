491f7cbfea8f10e4cf4b1e4e815bf166ca472d01
S-furi <stefano.furi@studio.unibo.it>
2023-11-10 12:33:02 +0100

    fix: general fix after last reading

--- listings/Serialization.kt
+++ listings/Serialization.kt
@@ -1,9 +1,7 @@
-fun <T : Any> encodeConcentration(content: T): String {
-    return runCatching {
+fun <T : Any> encodeConcentration(content: T): String =
+    runCatching {
         Json.Default
         .encodeToString(serializer(content::class.java), content)
     }.getOrElse {
         content.toString()
     }
-}
-
